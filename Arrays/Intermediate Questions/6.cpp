e4f8e298573efd4ecba67f8b5fc57d15851c6248
asp616848 <asp616848@gmail.com>
2024-01-21 12:17:39 +0530

    new question

--- Arrays/Intermediate Questions/6.cpp
+++ Arrays/Intermediate Questions/6.cpp
@@ -1,13 +1,32 @@
 #include<iostream>
 using namespace std;
 
-void getPriceArray(int price[], int n){
+//QUESTION - Maximum profit by buying and selling a share at most twice
 
+void getPriceArray(int price[], int n){
+    int profit[n];
+    for(int i = 0; i < n; i++){
+        profit[i] = 0;
+    }
+    int maxPrice = price[n-1];
+    for(int i = n-2; i >= 0; i--){
+        if(price[i] > maxPrice){
+            maxPrice = price[i];
+        }
+        profit[i] = max(profit[i+1], maxPrice - price[i]);
+    }
+    int minPrice = price[0];
+    for(int i = 1; i < n; i++){
+        if(price[i] < minPrice){
+            minPrice = price[i];
+        }
+        profit[i] = max(profit[i-1], profit[i] + (price[i] - minPrice));
+    }
+    cout << profit[n-1] << endl;
 }
 
 int main(){
     int price[] = {10, 22, 5, 75, 65, 80};
     int n = sizeof(price) / sizeof(price[0]);
     getPriceArray(price, n);
-    cout << n << endl;
 }
