3f99521345857d06b8f70d21ae6b1cb110d65e7e
asp616848 <asp616848@gmail.com>
2024-02-04 12:55:52 +0530

    restarted my dsa journey from basics

---
+++ Striver's 450/abc.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    //precompute:
+    map<int, int> mp;
+    for (int i = 0; i < n; i++) {
+        mp[arr[i]]++;
+    }
+
+    // iterate over the map:
+    /* for(auto it : mp){
+            cout << it.first << "->" << it.second << endl;
+        }
+    */
+
+    int q;
+    cin >> q;
+    while (q--) {
+        int number;
+        cin >> number;
+        // fetch:
+        cout << mp[number] << endl;
+    }
+    return 0;
+}
