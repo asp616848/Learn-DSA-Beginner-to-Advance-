3d7be2c877541c842365814ea1bebc4e40346758
Abhijeet Shashwat <abhijeet.s.ug23@plaksha.edu.in>
2024-06-01 09:18:52 +0530

    done

--- Striver's 450/Step-4-BinarySearch/on answers/Painters.C
+++ Striver's 450/Step-4-BinarySearch/on answers/Painters.C
@@ -2,32 +2,47 @@
 
 using namespace std;
 
+bool check(vector<int> arr, int mid, int k) {
+    int currentLoad = 0;
+
+    for (int i = 0; i < arr.size(); i++) {
+        if (currentLoad + arr[i] > mid) {
+            // Current painter cannot take more load, use a new painter
+            k--;
+            currentLoad = arr[i];
+
+            // If more painters are required than available, return false
+            
+        } else {
+            currentLoad += arr[i];
+        }
+    }
+    if (k <= 0) {
+                return false;
+            }
+    return true; // If all boards are painted with the given number of painters
+}
+
 int totalTime(vector<int> arr, int k){ //k is the number of painters amoung whom we have to devide
     int l = *max_element(arr.begin(), arr.end());
     int h = accumulate(arr.begin(), arr.end(),0);
-    int res = 0;
+    int res = h;
     while(l<=h){
-        int mid = (l+h)/2;
+        int mid = l + (h - l) / 2;
         if(check(arr,mid, k)){
             res = mid;
+            h = mid-1;
         }
-        
-    }
-}
-bool check(vector<int> arr,int mid, int k){
-    int currentLoad = 0;
-    int i = 0;
-    while(k){
-        if(currentLoad+arr[i] > mid){
-            currentLoad = 0;
-            k--;
-        }else{
-            currentLoad += arr[i];
+        else{
+            l = mid+1;
         }
-        i++;
     }
+    return res;
 }
 
-int main(){
-
+int main() {
+    vector<int> arr = {10, 20, 30, 40}; // Example lengths of boards
+    int k = 2; // Number of painters
+    cout << "Minimum time required: " << totalTime(arr, k) << endl;
+    return 0;
 }
