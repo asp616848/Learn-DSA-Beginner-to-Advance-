82b11ccb869f8e8abf5c2caae2a7f552315de405
Abhijeet Shashwat <asp616848@gmail.com>
2024-07-15 18:47:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (14.21%) - LeetSync

--- LeetCode/202-happy-number/happy-number.cpp
+++ LeetCode/202-happy-number/happy-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isHappy(int n) {
         unordered_map<int,bool> map;
-        while(n!= 1 || n!=7){
+        while(true){
             double sum = 0;
             while(n !=0){
                 sum += (n%10)*(n%10);
@@ -16,6 +16,5 @@ public:
             }
             n = sum;
         }
-        return true;
     }
 };
