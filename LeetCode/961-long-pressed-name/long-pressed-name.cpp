a5246434e9d298c9e04b8df2a5bb9235da39385d
Abhijeet Shashwat <asp616848@gmail.com>
2024-07-20 10:53:15 +0530

    Time: 4 ms (19.66%) | Memory: 7.8 MB (11.84%) - LeetSync

---
+++ LeetCode/961-long-pressed-name/long-pressed-name.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+        int i = 0, j = 0;
+        while (j < typed.length()) {
+            if (i < name.length() && name[i] == typed[j]) {
+                i++;
+            } else if (j == 0 || typed[j] != typed[j - 1]) {
+                return false;
+            }
+            j++;
+        }
+        return i == name.length();
+    }
+};
