d5ba0819cadbc23f0521827a20db65a146e46618
Abhijeet Shashwat <asp616848@gmail.com>
2024-08-14 19:42:17 +0530

    Time: 4 ms (89.97%) | Memory: 15.8 MB (39.87%) - LeetSync

--- LeetCode/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ LeetCode/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<int> st;
+        stack<int> stack;
+        std::vector<char> operators = {'+', '-', '/', '*'};
 
-        for (string c : tokens) {
-            if (c == "+") {
-                int second = st.top(); st.pop();
-                int first = st.top(); st.pop();
-                st.push(first + second);
-            } else if (c == "-") {
-                int second = st.top(); st.pop();
-                int first = st.top(); st.pop();
-                st.push(first - second);
-            } else if (c == "*") {
-                int second = st.top(); st.pop();
-                int first = st.top(); st.pop();
-                st.push(first * second);
-            } else if (c == "/") {
-                int second = st.top(); st.pop();
-                int first = st.top(); st.pop();
-                st.push(first / second);
-            } else {
-                st.push(stoi(c));
+        for(auto &i : tokens){
+            if (find(operators.begin(), operators.end(), i[0]) != operators.end()  && i.size() == 1){
+                int b = stack.top();
+                stack.pop();
+                int a = stack.top();
+                stack.pop();
+                int c ;
+                switch(i[0]){
+                    case '+':{
+                        c = (a + b);
+                        break;
+                    }
+
+                    case '-':{
+                        c = (a - b);
+                        break;
+
+                    }
+                    case '/':{
+                        c = (a / b);
+                        break;
+
+                    }
+                    case '*':{
+                        c = (a * b);
+                        break;
+
+                    }
+                }
+                stack.push(c);
+            }else{
+                stack.push(stoi(i));
             }
         }
-
-        return st.top();        
+        return stack.top();
     }
 };
