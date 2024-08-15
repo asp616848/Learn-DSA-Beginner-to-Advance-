914ea2a845805f62eeb276e9e4a913d037010062
Abhijeet Shashwat <asp616848@gmail.com>
2024-08-15 11:20:15 +0530

    Time: 159 ms (85.58%) | Memory: 137.3 MB (52.32%) - LeetSync

--- LeetCode/239-sliding-window-maximum/sliding-window-maximum.cpp
+++ LeetCode/239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -9,22 +9,20 @@ public:
         deque<int> maxDeque; // To keep maximums
 
         for (int i = 0; i < nums.size(); i++) {
-            // Remove front if it's outside the current window
-            if (i >= k && !maxDeque.empty() && maxDeque.front() == nums[i - k]) {
+            
+            if (i - maxDeque.front() == k  && !maxDeque.empty()) {
                 maxDeque.pop_front();
             }
 
-            // Remove all elements smaller than the current element from the back
-            while (!maxDeque.empty() && maxDeque.back() < nums[i]) {
+
+            while (!maxDeque.empty() && nums[maxDeque.back()] < nums[i]) {
                 maxDeque.pop_back();
             }
 
-            // Push current value to the deque
-            maxDeque.push_back(nums[i]);
+            maxDeque.push_back(i);
 
-            // Add front of deque to results when the first window of size k is complete
             if (i >= k - 1) {
-                out.push_back(maxDeque.front());
+                out.push_back(nums[maxDeque.front()]);
             }
         }
 
