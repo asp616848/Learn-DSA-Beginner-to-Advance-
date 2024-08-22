399648e67360d6db26e5c3121d307d311bf3e8a1
Abhijeet Shashwat <asp616848@gmail.com>
2024-08-22 11:37:21 +0530

    Time: 1734 ms (5.02%) | Memory: 77 MB (8.19%) - LeetSync

--- LeetCode/128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ LeetCode/128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    int getLeftLength(int a, unordered_map<int, bool>& hm) {
-        if (!hm[a]) {
-            return 0;
-        }
-        hm[a] = false;
-        return 1 + getLeftLength(a - 1, hm);
-    }
-
-    int getRightLength(int a, unordered_map<int, bool>& hm) {
-        if (!hm[a]) {
-            return 0;
-        }
-        hm[a] = false;
-        return 1 + getRightLength(a + 1, hm);
-    }
-
     int longestConsecutive(vector<int>& nums) {
         unordered_map<int, bool> hm;
         int maxi = 0;
+
+        // Populate the map with all numbers
         for (int num : nums) {
             hm[num] = true;
         }
         
+        // Iterate through each number and process only if it's the start of a sequence
         for (int num : nums) {
-            if (hm[num]) {
-                int leftLen = getLeftLength(num - 1, hm);
-                int rightLen = getRightLength(num + 1, hm);
-                int totalLen = leftLen + 1 + rightLen; // Including current num
-                maxi = max(totalLen, maxi);
+            if (!hm[num - 1]) {  // Only start if `num` is the start of a sequence
+                int currentNum = num;
+                int length = 1;
+
+                // Check the consecutive sequence length
+                while (hm[currentNum + 1]) {
+                    currentNum++;
+                    length++;
+                }
+
+                // Update the maximum length found
+                maxi = max(maxi, length);
             }
         }
+
         return maxi;
     }
 };
