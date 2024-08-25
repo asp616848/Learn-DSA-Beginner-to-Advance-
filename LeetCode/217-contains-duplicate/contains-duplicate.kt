2e44553035073f04d8a4a777e305ace58435370d
Abhijeet Shashwat <asp616848@gmail.com>
2024-08-25 22:09:19 +0530

    Time: 433 ms (50.62%) | Memory: 50.5 MB (72.41%) - LeetSync

--- LeetCode/217-contains-duplicate/contains-duplicate.kt
+++ LeetCode/217-contains-duplicate/contains-duplicate.kt
@@ -2,7 +2,7 @@ class Solution {
     fun containsDuplicate(nums: IntArray): Boolean {
         val map = mutableMapOf<Int, Boolean>()
         for (i in nums){
-            if(map.get(i) == true){
+            if(map.containsKey(i)){
                 return true
             }
             map.put(i, true)
