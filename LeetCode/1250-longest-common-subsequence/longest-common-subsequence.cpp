44bb1538dcfacf6833c54481a9b4afccc890c9cb
Abhijeet Shashwat <asp616848@gmail.com>
2025-01-12 08:54:24 +0530

    Time: 13 ms (93.73%) | Memory: 9.1 MB (90.75%) - LeetSync

--- LeetCode/1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ LeetCode/1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -3,18 +3,22 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.length();
         int m = text2.length();
-        vector<vector<int>> vec(n+1, vector<int>(m+1, 0)); // each element is initiated as 0
-
-        for(int i = 1; i <=n; i++){
-            for(int j = 1; j <= m; j++){
-                if(text1[i-1] == text2[j-1]){
-                    vec[i][j] = vec[i-1][j-1] + 1;
-                }
-                else{
-                    vec[i][j] = max(vec[i-1][j], vec[i][j-1]);
+        
+        // Initialize two arrays to store the DP values for the current and previous rows
+        vector<int> prev(m + 1, 0), curr(m + 1, 0);
+        
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(text1[i - 1] == text2[j - 1]) {
+                    curr[j] = prev[j - 1] + 1;
+                } else {
+                    curr[j] = max(prev[j], curr[j - 1]);
                 }
             }
+            // Swap the current and previous row
+            prev = curr;
         }
-        return vec[n][m];
+        
+        return prev[m];
     }
 };
