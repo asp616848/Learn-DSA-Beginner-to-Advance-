9ad1285e689da58473c3d6b00a6d99b9a869997e
Abhijeet Shashwat <asp616848@gmail.com>
2025-01-24 07:55:26 +0530

    Time: 23 ms (99.03%) | Memory: 101.8 MB (24.78%) - LeetSync

--- LeetCode/452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ LeetCode/452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-    static bool comp(vector<int> pointa, vector<int> pointb){
-        return pointa[1] < pointb[1];
+    static bool comp(const pair<int, int>& pointa, const pair<int, int>& pointb) {
+        return pointa.second < pointb.second;  // Compare the second element (right endpoint)
     }
+    
     int findMinArrowShots(vector<vector<int>>& points) {
         if (points.empty()) return 0;
-        sort(points.begin(), points.end(), comp);
+
+        // Convert the points into a vector of pairs
+        vector<pair<int, int>> intervals;
+        for (const auto& p : points) {
+            intervals.push_back({p[0], p[1]});
+        }
+
+        // Sort intervals based on the second value (right endpoint)
+        sort(intervals.begin(), intervals.end(), comp);
 
         int arrows = 1;
-        int ind = points[0][1];
-        for(int i = 1; i < points.size(); i++){
-            if(points[i][0] > ind){
+        int ind = intervals[0].second;  // The first arrow is shot at the end of the first interval
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i].first > ind) {  // No overlap, shoot a new arrow
                 arrows++;
-                ind = points[i][1];
+                ind = intervals[i].second;  // Move the arrow to the new endpoint
             }
         }
         return arrows;
